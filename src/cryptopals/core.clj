e558442e0872294dc2e2890084d18030573c9cd6
Matthew Batema <MBatema@trustwave.com>
2015-08-11 02:57:53 -0700

    Hex decoding and base64 encoding.

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,6 +1,59 @@
 (ns cryptopals.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def decode-hex-str
+  "Translate a hex string into the ordinal character
+  values (represented as Java Bytes)."
+  (comp (partial map (comp #(Integer/parseInt % 16)
+                           (partial apply str)))
+        (partial partition 2)))
+
+(defn- byte-accumulator
+  "Build up the value of multiple bytes."
+  [acc byte]
+  (+ byte
+     (bit-shift-left acc 8)))
+
+(def sixbit-extraction
+  "Extract 4 six-bit values from a 24-bit value."
+  (comp first
+        #(reduce (fn [[acc block] _]
+                   [(conj acc (bit-and block 63))
+                    (bit-shift-right block 6)])
+                 [(list) %]
+                 (range 4))))
+
+(def base64-characters
+  (concat (map char (range (byte \A)
+                           (inc (byte \Z))))
+          (map char (range (byte \a)
+                           (inc (byte \z))))
+          (map char (range (byte \0)
+                           (inc (byte \9))))
+          [\+ \/]))
+
+(defn encode-base64
+  [chars]
+  (let [;; Calculate the required amount of padding
+        padded-bytes (mod (count chars) 3)
+        replace-chars (mod (- 3 padded-bytes) 3)
+        padding (repeat replace-chars \=)
+        ;; Split up the payload into groups of 3
+        byte-blocks (partition 3 3 [0 0] chars)
+        ;; Translate each group into base64 characters
+        xfs (comp (map #(reduce byte-accumulator %))
+                  (mapcat sixbit-extraction)
+                  (map #(nth base64-characters %)))
+        ;; Drop the appropriate number of trailing "A"s
+        base64 (->> byte-blocks
+                    (sequence xfs)
+                    (drop-last replace-chars)
+                    vec)]
+    ;; Append appropriate amount of padding
+    (->> padding
+         (apply conj base64)
+         (apply str))))
+
+(def hex->base64
+  "Convert a hex encoded string to a base64 encoded string."
+  (comp encode-base64
+        decode-hex-str))
--- test/cryptopals/core_test.clj
+++ test/cryptopals/core_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [cryptopals.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;; http://cryptopals.com/sets/1/challenges/1/
+(deftest set-1-challenge-1
+  (testing "Convert hex to base64"
+    (is (= (->> "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+                decode-hex-str
+                (into-array Byte/TYPE)
+                javax.xml.bind.DatatypeConverter/printBase64Binary)
+           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))
+
+    (is (= (->> "Man"
+                .getBytes
+                encode-base64)
+           "TWFu"))
+
+    (is (= (->> "sure."
+                (iterate butlast)
+                (take-while (complement nil?))
+                (map #(map byte %))
+                (map encode-base64))
+           ["c3VyZS4=" "c3VyZQ==" "c3Vy" "c3U=" "cw=="]))
+
+    (is (= (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))))
